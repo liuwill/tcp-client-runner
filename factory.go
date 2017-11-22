53dc24ad18f57d4025d22b02a4d07f1b4623263f
liuwill <liuwei_will@qq.com>
2017-11-22 15:44:55 +0800

    add invoker at right place

--- factory.go
+++ factory.go
@@ -57,40 +57,6 @@ func (factory *GameCommander) CreateGeneralCommand() Command {
 	return &GeneralCommand{}
 }
 
-// ** 创建游戏命令 ** //
-
-// ** 执行游戏命令 ** //
-type Invoker struct {
-	command Command
-}
-
-func (invoker *Invoker) SetCommand(command Command) {
-	invoker.command = command
-}
-
-func (invoker *Invoker) Action(input string) {
-	invokerData := invoker.Parse(input, invoker.command)
-	invoker.command.Execute(invokerData)
-}
-func (invoker *Invoker) Parse(input string, command Command) map[string]string {
-	input = strings.Replace(input, "  ", " ", -1)
-	inputPiece := strings.Split(input, " ")
-	result := make(map[string]string)
-
-	for index, field := range command.Fields() {
-		if index < len(inputPiece) && len(inputPiece[index]) > 0 {
-			result[field] = inputPiece[index]
-		}
-	}
-
-	if len(command.Fields()) == 0 {
-		result["content"] = strings.Join(inputPiece[1:], " ")
-		return result
-	}
-
-	return result
-}
-
 func startEmitter(messageEmitter chan []byte) {
 	for {
 		message := <-messageEmitter
--- runner.go
+++ runner.go
@@ -18,3 +18,34 @@ func ReadLine(tip string, def string) string {
 	}
 	return strings.TrimSpace(input)
 }
+
+type Invoker struct {
+	command Command
+}
+
+func (invoker *Invoker) SetCommand(command Command) {
+	invoker.command = command
+}
+
+func (invoker *Invoker) Action(input string) {
+	invokerData := invoker.Parse(input, invoker.command)
+	invoker.command.Execute(invokerData)
+}
+func (invoker *Invoker) Parse(input string, command Command) map[string]string {
+	input = strings.Replace(input, "  ", " ", -1)
+	inputPiece := strings.Split(input, " ")
+	result := make(map[string]string)
+
+	for index, field := range command.Fields() {
+		if index < len(inputPiece) && len(inputPiece[index]) > 0 {
+			result[field] = inputPiece[index]
+		}
+	}
+
+	if len(command.Fields()) == 0 {
+		result["content"] = strings.Join(inputPiece[1:], " ")
+		return result
+	}
+
+	return result
+}
